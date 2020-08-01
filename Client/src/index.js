ca3410ef95bdedfda43134aadb87c9e975b464f9
Gshopping <zays.kim@gmail.com>
2020-08-02 00:00:31 +0900

    Adding redux, react-redux, middlewares : redux-promise(promise), redux-thunk(function), root_reducer using combineReducers

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
+
+import {Provider} from 'react-redux'
+
+import 'antd/dist/antd.css'; //ant design css framework
+import { applyMiddleware , createStore} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,thunkMiddleware)(createStore)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={
+    createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+
+  }>
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
